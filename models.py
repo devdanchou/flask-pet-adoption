7ad9ae5d465723a511dcbe1f3072153a95be0917
Daniel Chou <daniel.cw.chou@gmail.com>
2023-07-28 14:41:32 -0400

    WIP Step 2

--- models.py
+++ models.py
@@ -14,3 +14,38 @@ def connect_db(app):
     app.app_context().push()
     db.app = app
     db.init_app(app)
+
+class Pet(db.Model):
+    """pets to be adopted"""
+
+    __tablename__ = "pets"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True)
+
+    name = db.Column(
+        db.String(50),
+        nullable=False)
+
+    species = db.Column(
+        db.String(50),
+        nullable=False)
+
+    photo_url = db.Column(
+        db.Text,
+        nullable=False,
+        default="")
+
+    age = db.Column(
+        db.String(50),
+        nullable=False) //TODO: need to include age list
+
+    notes = db.Column(
+        db.Text)
+
+    available = db.Column(
+        db.Boolean,
+        nullable=False,
+        default=True)
+
